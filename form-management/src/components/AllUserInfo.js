6f46e122defe041e771d626ce3ec33189c9d7524
Tyrone Smith <billionairefly.ts@gmail.com>
2019-08-09 13:26:22 -0400

    created useState hook for UserInfoList

---
+++ form-management/src/components/AllUserInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllUserInfo = (props) => {
+    return (
+        <div>
+            <h3>{props.UserInfoList.user}</h3>
+        </div>
+    );
+};
+
+export default AllUserInfo;
---
+++ form-management/src/components/UserInfoList.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import UserInfoList from "./UserInfoList";
+import AllUserInfo from "./AllUserInfo";
+import axiosWithAuth from "../auth";
+
+UserInfoList = (props) => {
+    const [state, setState] = useState()
+
+    useEffect(() => {
+        axiosWithAuth.
+
+    })
+}
